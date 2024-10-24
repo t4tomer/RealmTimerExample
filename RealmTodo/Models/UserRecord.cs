0766533c256a422e7ec36bf76ffb65c976b38e44
t4tomer <tomer.polsky27@gmail.com>
2024-10-24 20:27:46 +0300

    I added UserRecord class and orginized to folders
    I added folders for the ItemViews and ItemViewModels

---
+++ RealmTodo/Models/UserRecord.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using Realms;
+using RealmTodo.Services;
+
+namespace RealmTodo.Models
+{
+    public partial class UserRecord : IRealmObject
+    {
+        [PrimaryKey]
+        [MapTo("_id")]
+        public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+
+        [MapTo("owner_id")]
+        [Required]
+        public string OwnerId { get; set; }
+
+        [MapTo("summary")]
+        [Required]
+        public string Summary { get; set; }
+
+        [MapTo("mapName")]
+        public string MapName { get; set; }
+
+        [MapTo("trackTime")]
+        public string TrackTime { get; set; }
+
+
+        [MapTo("uploadDate")]
+        public string UploadDate { get; set; }
+
+        public bool IsMine => OwnerId == RealmService.CurrentUser.Id;
+    }
+}
